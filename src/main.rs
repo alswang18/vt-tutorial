e9c2cd318153d6c0147055510a0a1ab9ed83833f
alswang18 <alec.sy.wang@gmail.com>
2025-09-08 22:18:04 -0700

    added vulkan validation

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,10 @@ use std::os::raw::c_void;
 use vulkanalia::vk::ExtDebugUtilsExtension;
 
 const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
+const VALIDATION_ENABLED: bool = cfg!(debug_assertions);
+
+const VALIDATION_LAYER: vk::ExtensionName =
+    vk::ExtensionName::from_bytes(b"VK_LAYER_KHRONOS_validation");
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
@@ -88,37 +92,57 @@ impl App {
     }
 
     unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
-        let application_info = vk::ApplicationInfo::builder()
-            .application_name(b"Vulkan Tutorial\0")
-            .application_version(vk::make_version(1, 0, 0))
-            .engine_name(b"No Engine\0")
-            .engine_version(vk::make_version(1, 0, 0))
-            .api_version(vk::make_version(1, 0, 0));
+        unsafe {
+            let application_info = vk::ApplicationInfo::builder()
+                .application_name(b"Vulkan Tutorial\0")
+                .application_version(vk::make_version(1, 0, 0))
+                .engine_name(b"No Engine\0")
+                .engine_version(vk::make_version(1, 0, 0))
+                .api_version(vk::make_version(1, 0, 0));
 
-        let mut extensions = vk_window::get_required_instance_extensions(window)
-            .iter()
-            .map(|e| e.as_ptr())
-            .collect::<Vec<_>>();
+            let available_layers = entry
+                .enumerate_instance_layer_properties()?
+                .iter()
+                .map(|l| l.layer_name)
+                .collect::<HashSet<_>>();
 
-        // Required by Vulkan SDK on macOS since 1.3.216.
-        let flags = if cfg!(target_os = "macos") && entry.version()? >= PORTABILITY_MACOS_VERSION {
-            info!("Enabling extensions for macOS portability.");
-            extensions.push(
-                vk::KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_EXTENSION
-                    .name
-                    .as_ptr(),
-            );
-            extensions.push(vk::KHR_PORTABILITY_ENUMERATION_EXTENSION.name.as_ptr());
-            vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
-        } else {
-            vk::InstanceCreateFlags::empty()
-        };
+            if VALIDATION_ENABLED && !available_layers.contains(&VALIDATION_LAYER) {
+                return Err(anyhow!("Validation layer requested but not supported."));
+            }
 
-        let info = vk::InstanceCreateInfo::builder()
-            .application_info(&application_info)
-            .enabled_extension_names(&extensions)
-            .flags(flags);
-        Ok(unsafe { entry.create_instance(&info, None)? })
+            let layers = if VALIDATION_ENABLED {
+                vec![VALIDATION_LAYER.as_ptr()]
+            } else {
+                Vec::new()
+            };
+
+            let mut extensions = vk_window::get_required_instance_extensions(window)
+                .iter()
+                .map(|e| e.as_ptr())
+                .collect::<Vec<_>>();
+
+            // Required by Vulkan SDK on macOS since 1.3.216.
+            let flags =
+                if cfg!(target_os = "macos") && entry.version()? >= PORTABILITY_MACOS_VERSION {
+                    info!("Enabling extensions for macOS portability.");
+                    extensions.push(
+                        vk::KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_EXTENSION
+                            .name
+                            .as_ptr(),
+                    );
+                    extensions.push(vk::KHR_PORTABILITY_ENUMERATION_EXTENSION.name.as_ptr());
+                    vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+                } else {
+                    vk::InstanceCreateFlags::empty()
+                };
+
+            let info = vk::InstanceCreateInfo::builder()
+                .application_info(&application_info)
+                .enabled_extension_names(&extensions)
+                .enabled_layer_names(&layers)
+                .flags(flags);
+            Ok(entry.create_instance(&info, None)?)
+        }
     }
 }
 
